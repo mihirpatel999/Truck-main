dc7f195ec9f4b37bf6e6d63ba34efa815d7c8018
mihirpatel999 <mihirpatel9991@gmail.com>
2025-07-08 16:12:26 +0800

    dffff

--- src/PlantMaster.jsx
+++ src/PlantMaster.jsx
@@ -2370,7 +2370,7 @@ export default function PlantMaster({ onClose }) {
     if (onClose) {
       onClose();
     } else {
-      navigate('/'); // Navigate to home if no onClose prop provided
+      navigate('/home'); // Navigate to home if no onClose prop provided
     }
   };
 
